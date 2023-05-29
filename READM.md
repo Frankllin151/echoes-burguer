c80f0a6fd1677c034f7bdf51f54be728c7a1c95f
Frankllin151 <frankllinsilva300@gamil.com>
2023-05-29 15:48:39 -0300

    first commit

---
+++ READM.md
@@ -0,0 +1,1 @@
+* Echoies kitchen and a web aplication to hamburger shop service provider
---
+++ assets/js/home.js
@@ -0,0 +1,9 @@
+// menu renposive 
+
+const btn_mobile = document.getElementById('btn-mobile');
+
+
+btn_mobile.addEventListener('click' , (e) =>{
+    document.querySelector('#menu-rp').classList.toggle('show');
+    document.querySelector('.fa-bars').classList.toggle('fa-x');
+});

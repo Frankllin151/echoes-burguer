8a3d90598b4892960e897002597c510dd2fe9913
Frankllin151 <frankllinsilva300@gamil.com>
2023-05-30 15:52:03 -0300

    room comit

--- assets/js/home.js
+++ assets/js/home.js
@@ -7,3 +7,19 @@ btn_mobile.addEventListener('click' , (e) =>{
     document.querySelector('#menu-rp').classList.toggle('show');
     document.querySelector('.fa-bars').classList.toggle('fa-x');
 });
+
+
+function UpdateMobileWidth() {
+    var widthLargura = window.innerWidth;
+
+    if (widthLargura < 414) {
+        document.querySelector('#btn-hym').classList.add('btn-gh');
+        document.querySelector('#btn-hym').classList.remove('email-btn');
+    } else {
+        document.querySelector('#btn-hym').classList.remove('btn-gh');
+        document.querySelector('#btn-hym').classList.add('email-btn');
+    }
+}
+
+UpdateMobileWidth();
+
